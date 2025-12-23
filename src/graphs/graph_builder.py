9d2332a29c81d8154e8716e6c436bf0c08dba1cb
Vivek Kumar Gupta <vivek2.230104023@iiitbh.ac.in>
2025-12-23 22:29:41 +0530

    node implementation is done for route language

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -24,9 +24,43 @@ class GraphBuilder:
         return self.graph
     
     
+    def build_language_graph(self):
+        """
+        Build a graph for blog generation with inputs topic and language
+        """
+        self.blog_node_obj=BlogNode(self.llm)
+        print(self.llm)
+        ## Nodes
+        self.graph.add_node("title_creation", self.blog_node_obj.title_creation)
+        self.graph.add_node("content_generation",self.blog_node_obj.content_generation)
+        self.graph.add_node("hindi_translation",lambda state: self.blog_node_obj.translation({**state, "current_language": "hindi"}))
+        self.graph.add_node("french_translation",lambda state: self.blog_node_obj.translation({**state, "current_language": "french"}))
+        self.graph.add_node("route",self.blog_node_obj.route)
+
+        ## edges and conditional edges
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", "route")
+        
+        self.graph.add_conditional_edges(
+            "route",
+            self.blog_node_obj.route_decision,
+            {
+                "hindi":"hindi_translation",
+                "french":"french_translation"
+            }
+        )
+        self.graph.add_edge("hindi_translation",END)
+        self.graph.add_edge("french_translation",END)
+        return self.graph
+
+    
     def setup_graph(self,usecase):
         if usecase=="topic":
             self.build_topic_graph()
+        if usecase=="language":
+            print("Language block")
+            self.build_language_graph()
 
         return self.graph.compile()
     
