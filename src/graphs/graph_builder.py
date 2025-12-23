e11acf3b7205b2ddfe5eca2194dd7bbe67e7c23b
Vivek Kumar Gupta <vivekgupta3749@gmail.com>
2025-12-23 21:57:36 +0530

    Integrating langgraph stdio for tracking

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -29,3 +29,10 @@ class GraphBuilder:
             self.build_topic_graph()
 
         return self.graph.compile()
+    
+## Below code is for the langsmith langgraph studio
+llm=GroqLLM().get_llm()
+
+## get the graph
+graph_builder=GraphBuilder(llm)
+graph=graph_builder.build_topic_graph().compile()    
